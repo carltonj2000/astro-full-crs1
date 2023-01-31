ac84254a16990b83b9f485d60cfb2bcc0228b94d
Carlton Joseph <carlton.joseph@gmail.com>
2023-01-30 16:04:02 -0800

    add routing

--- src/data/navData.js
+++ src/data/navData.js
@@ -1,6 +1,6 @@
 const navData = [
-  { name: "About", path: "/about" },
-  { name: "Blog", path: "/Blog" },
+  { name: "About", path: "/about/" },
+  { name: "Blog", path: "/blog/" },
 ];
 
 export default navData;
