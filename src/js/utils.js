657670988a65be93ae9b751eb63f744c26a0c895
Carlton Joseph <carlton.joseph@gmail.com>
2023-01-30 23:17:02 -0800

    add filter and sort post

--- src/js/utils.js
+++ src/js/utils.js
@@ -14,3 +14,32 @@ export function formatDate(date) {
     timeZone: "UTC",
   });
 }
+
+export function formatBlogPosts(
+  posts,
+  {
+    filterOutDrafts = true,
+    filterOutFuture = true,
+    sortByDate = true,
+    limit = undefined,
+  } = {}
+) {
+  let filteredPosts = posts.reduce((acc, post) => {
+    const { date, draft } = post.frontmatter;
+    if (filterOutDrafts && draft) return acc;
+    if (filterOutFuture && new Date(date) > new Date()) return acc;
+    acc.push(post);
+    return acc;
+  }, []);
+  if (sortByDate) {
+    filteredPosts.sort(
+      (a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date)
+    );
+  } else {
+    filteredPosts.sort(() => Math.randoom() - 0.5);
+  }
+  if (typeof limit === "number") {
+    filteredPosts = filteredPosts.slice(0, limit);
+  }
+  return filteredPosts;
+}
